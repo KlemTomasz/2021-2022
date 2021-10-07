2b646875012654d13d2e8f405cf520756a15f359
KlemTomasz <89963278+KlemTomasz@users.noreply.github.com>
2021-10-07 08:44:28 +0200

    Add files via upload

--- 4_3_form_ageavg.php
+++ 4_3_form_ageavg.php
@@ -5,15 +5,16 @@
     <title>Średni wiek</title>
   </head>
   <body>
-
-
       <?php
-        if (!isset($_POST['person'])){
+      if (isset($_GET['home'])) {
+        echo 'Witaj ponownie!';
+      }
+        if (!isset($_POST['person']) && !isset($_POST['buttonAvg'])){
           echo <<< FORMCOUNTPERSON
           <h3>Ilość osób w rodzinie</h3>
           <form method="post">
-            <input type="number" name="person" placeholder="Ilość osób">
-            <input type="submit" value="Zatwierdź">
+            <input type="number" name="person" placeholder="Ilość osób"><br><br>
+            <input type="submit" value="Zatwierdź"><hr>
           </form>
 FORMCOUNTPERSON;
         }
@@ -27,10 +28,26 @@ FORMCOUNTPERSON;
                 echo "<input type='number' name='person$i' placeholder='Wiek osoby nr $i'><br><br>";
               }
           echo <<< FORMCOUNTPERSON
-            <input type="submit" value="Zatwierdź">
+            <input type="submit" name="buttonAvg" value="Zatwierdź">
           </form>
 FORMCOUNTPERSON;
         }
+        if (isset($_POST['buttonAvg'])) {
+          echo "<h3>Średni wiek</h3>";
+          //print_r($_POST);
+          $avg=0;
+          $count=0;
+          foreach ($_POST as $key => $value) {
+            //echo "$key: $value<br>";
+            if ($key != 'buttonAvg') {
+              $avg=$avg+$value;
+              $count++;
+            }
+          }
+          echo "Średni wiek wynosi: ".number_format($avg/$count, 2)." lat<hr>";
+        }
+        echo '<a href="./4_3_form_ageavg.php?home=">Strona główna</a>';
       ?>
+
   </body>
 </html>
